c6c52f7465a9f6a783d50ebb6eddff14c8993ea8
darkant99 <darkant99@naver.com>
2021-08-26 11:33:56 +0900

    Initial commit

---
+++ src/main/java/com/shop/franchise/ShopFranchiseApplication.java
@@ -0,0 +1,13 @@
+package com.shop.franchise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopFranchiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopFranchiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/franchise/ShopFranchiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.franchise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopFranchiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
